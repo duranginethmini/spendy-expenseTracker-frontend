7ffd762e727ab0dae51d91cc29c54ac2a76abcaa
duranginethmini <durangineth02@gmail.com>
2025-03-11 16:07:25 +0530

    "login created "

---
+++ budget-tracker/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                display: ["Poppins", "sans-serif"],
+            },
+            colors: {
+                primary: "#875cf5",
+            },
+            screens: {
+                "3xl": "1920px",
+            },
+        },
+    },
+    plugins: [],
+};
