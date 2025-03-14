70ea506a3aabfd4fe08b3946631b4d72b4e43374
duranginethmini <durangineth02@gmail.com>
2025-03-14 22:41:35 +0530

    " recent transaction component completed

--- budget-tracker/src/pages/dashboard/Homepage.jsx
+++ budget-tracker/src/pages/dashboard/Homepage.jsx
@@ -1,13 +1,14 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import DashboardLayout from "../../components/layouts/DashboardLayout.jsx";
-import {UseUserAuth} from "../../hooks/UseUserAuth.jsx";
-import {useNavigate} from "react-router-dom";
+import { UseUserAuth } from "../../hooks/UseUserAuth.jsx";
+import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance.js";
-import {API_PATHS} from "../../utils/apiPath.js";
-import {IoMdCard} from "react-icons/io";
+import { API_PATHS } from "../../utils/apiPath.js";
+import { IoMdCard } from "react-icons/io";
 import InfoCard from "../../components/Cards/InfoCard.jsx";
-import {LuHandCoins,LuWalletMinimal} from "react-icons/lu";
-import {addThousandsSeparator} from "../../utils/helpers.js";
+import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
+import { addThousandsSeparator } from "../../utils/helpers.js";
+import RecentTransactions from "../../components/Dashboard/RecentTransactions.jsx";
 
 const Home = () => {
     UseUserAuth();
@@ -17,15 +18,15 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
 
     const fetchDashboardData = async () => {
-        if(loading) return;
+        if (loading) return;
 
         setLoading(true);
 
-        try{
+        try {
             const response = await axiosInstance.get(
-                    `${API_PATHS.DASHBOARD.GET_DATA}`
+                `${API_PATHS.DASHBOARD.GET_DATA}`
             );
-            if(response.data) {
+            if (response.data) {
                 setDashboardData(response.data);
             }
         } catch (error) {
@@ -34,34 +35,41 @@ const Home = () => {
             setLoading(false);
         }
     };
+
     useEffect(() => {
         fetchDashboardData();
-        return () => {}
-    },[])
+    }, []);
+
     return (
         <DashboardLayout activeMenu="Dashboard">
-           <div className = "my-5 mx-auto">
-               <div className="grid grid-cols md:grid-cols-3 gap-6">
-                   <InfoCard
-                       icon = {<IoMdCard/>}
-                       label = "Total Balance"
-                       value = {addThousandsSeparator(dashboardData?.totalBalance || 0 )}
-                       color = "bg-primary"
-                       />
-                   <InfoCard
-                       icon = {<IoMdCard/>}
-                       label = "Total Income"
-                       value = {addThousandsSeparator(dashboardData?.totalBalance || 0 )}
-                       color = "bg-orange-500"
-                   />
-                   <InfoCard
-                       icon = {<IoMdCard/>}
-                       label = "Total Expense"
-                       value = {addThousandsSeparator(dashboardData?.totalBalance || 0 )}
-                       color = "bg-red-500"
-                   />
-               </div>
-           </div>
+            <div className="my-5 mx-auto">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    <InfoCard
+                        icon={<IoMdCard />}
+                        label="Total Balance"
+                        value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
+                        color="bg-primary"
+                    />
+                    <InfoCard
+                        icon={<LuWalletMinimal />}
+                        label="Total Income"
+                        value={addThousandsSeparator(dashboardData?.totalIncome || 0)}
+                        color="bg-orange-500"
+                    />
+                    <InfoCard
+                        icon={<LuHandCoins />}
+                        label="Total Expense"
+                        value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
+                        color="bg-red-500"
+                    />
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+                    <RecentTransactions
+                        transactions={dashboardData?.recentTransactions}
+                        onSeeMore={() => navigate("/expense")}
+                    />
+                </div>
+            </div>
         </DashboardLayout>
     );
 };
