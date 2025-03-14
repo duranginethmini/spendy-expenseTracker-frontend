e0b7507703a664f90fa774ca7e052f3e66b6cbc6
duranginethmini <durangineth02@gmail.com>
2025-03-14 13:19:32 +0530

    " sidebar completed  "

--- budget-tracker/src/components/layouts/DashboardLayout.jsx
+++ budget-tracker/src/components/layouts/DashboardLayout.jsx
@@ -1,24 +1,27 @@
-import React, { useContext } from 'react';
-import Navbar  from "./Navbar.jsx";
+import React, { useContext } from "react";
+import Navbar from "./Navbar.jsx";
 import { UserContext } from "../../context/UserContext.jsx";
-import { Sidemenu } from "./Sidemenu.jsx";
+import Sidemenu from "./Sidemenu.jsx";
 
 const DashboardLayout = ({ children, activeMenu }) => {
     const { user } = useContext(UserContext);
+
+    console.log("User context:", user); // Debugging
+
     return (
-        <div className="">
+        <div>
             <Navbar activeMenu={activeMenu} />
 
             {user && (
                 <div className="flex">
-                    <div className="max-[1080px]:hidden">
+                    <div className="hidden md:block">
                         <Sidemenu activeMenu={activeMenu} />
                     </div>
                     <div className="grow mx-5">{children}</div>
                 </div>
             )}
         </div>
-    )
-}
+    );
+};
 
 export default DashboardLayout;
--- budget-tracker/src/pages/dashboard/Homepage.jsx
+++ budget-tracker/src/pages/dashboard/Homepage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import DashboardLayout from "../../components/layouts/DashboardLayout.jsx";
+import {UseUserAuth} from "../../hooks/UseUserAuth.jsx";
 
 const Home = () => {
+    UseUserAuth();
     return (
         <DashboardLayout activeMenu="Dashboard">
             <div className="my-5 mx-auto w-full text-center text-gray-600">
